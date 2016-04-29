7add11f2972c07522018626dbeb119d5b38b695b
Martin Salias <martin@salias.com.ar>
2016-04-29 07:54:10 -0300

    Put state in a session

--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
 require 'sinatra'
 require_relative './lib/motor.rb'
 
+enable :sessions
+
 get '/' do
-  @@motor = Motor.new "backlog"
+  motor = Motor.new "backlog"
   @resultado = ""
+  session[:motor] = motor
   erb :portada
 end
 
 post '/intento' do
+  motor = session[:motor]
   @letra = params[:letra]
-  @@motor.intentar @letra
-  @vidas = @@motor.vidas
-  @display = @@motor.display
+  motor.intentar @letra
+  @vidas = motor.vidas
+  @display = motor.display
   erb :portada
 end
